8ba977df17511a61e466144971ffe2ad6dee0f8b
Luis <luis.felipe@rcatechnology.com.br>
2022-09-30 22:23:32 -0300

    Avaliação

---
+++ luisfelipe-d7-avaliacao/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace luisfelipe_d7_avaliacao
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+           
+        }
+
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }
+    }
+}
+
---
+++ luisfelipe-d7-avaliacao/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace luisfelipe_d7_avaliacao
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        
+        public MainWindow()
+        {
+          
+            InitializeComponent();
+        }
+
+        private void Acessar(object sender, RoutedEventArgs e)
+        {
+            string email;
+            string senha;
+            email = user.Text;
+            senha = pass.Password.ToString();
+            if (email != "admin@email.com" && senha != "admin123") 
+            {
+                MessageBox.Show("Credenciais inválidas.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else 
+            {
+                MessageBox.Show("Usuário autenticado!", "Sucesso", MessageBoxButton.OK, MessageBoxImage.None);
+            }
+            
+        }
+    }
+}
